9f81bae683c977b65851b06214ee8f767b8d2de3
Akshay Vs <akshay.vs2005@gmail.com>
2023-04-03 11:01:03 +0530

    Upload page: front end design completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import ImageUploader from './components/imageUploader/imageUploader';
+import Header from './components/header/header';
 import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <ImageUploader></ImageUploader>
-      
     </div>
   );
 }
